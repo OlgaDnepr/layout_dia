a505f7cc39721c1f561f67378ca4b1943fe934f1
Your Name <cocochoco.story@gmail.com>
2025-08-21 19:59:34 +0300

    make build

---
+++ layout_dia.8f387d1e.js
@@ -0,0 +1,2 @@
+"use strict";document.addEventListener("DOMContentLoaded",function(){document.querySelectorAll("textarea").forEach(t=>{t.style.height="auto",t.style.height=t.scrollHeight+"px",t.addEventListener("input",function(){this.style.height="auto",this.style.height=this.scrollHeight+"px"})})});
+//# sourceMappingURL=layout_dia.8f387d1e.js.map
